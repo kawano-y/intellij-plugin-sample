bd005e4cffbc74d5f85f5864c4d2619eae5e25bd
GitHub Action <action@github.com>
2021-12-14 03:25:31 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/kawanoy/intellijpluginsample/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.kawanoy.intellijpluginsample.services
+
+import com.github.kawanoy.intellijpluginsample.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/kawanoy/intellijpluginsample/services/MyProjectService.kt
+++ src/main/kotlin/com/github/kawanoy/intellijpluginsample/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.kawanoy.intellijpluginsample.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.kawanoy.intellijpluginsample.MyBundle
 
 class MyProjectService(project: Project) {
 
